8dfec05c4c2471a5ad61d279245acb0f500e3210
Marfuen <marfuen98@gmail.com>
2019-02-12 17:16:55 -0500

    Trying to fix heroku deployment

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
+let DATABASE_URL = process.env.DATABASE_URL
 
-console.log(typeof process.env.DATABASE_URL)
-module.exports = new Sequelize(process.env.DATABASE_URL, {
+module.exports = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
 });
